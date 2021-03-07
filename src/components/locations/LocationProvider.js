69ac62c618d8108b2dd1b074d4ef3dc8f982a4d6
nicoletatum <nicoletatum@outlook.com>
2021-03-06 22:59:18 -0600

    first commit

---
+++ src/components/locations/LocationProvider.js
@@ -0,0 +1,9 @@
+import React, { useState, createContext } from "react"
+
+export const LocationContext = createContext()
+
+export const LocationProvier = (props) => {
+    const [locations, setLocations] = useState([])
+
+    const getLocations = ()
+}
